2b20f85fccd0054e0225512253a07e4598eaf15f
mishohan <mishohan.o786@gmail.com>
2025-08-02 19:53:12 +0800

    Use of for instead of for...of loop

---
+++ script4.js
@@ -0,0 +1,15 @@
+const cats = ["Pete", "Biggles", "Jasmine"];
+
+let myFavoriteCats = "My cats are called ";
+
+for(let i = 0; i < cats.length; i++){
+    if(i === cats.length - 1){
+
+        myFavoriteCats += `and ${cats[i]}.`;
+    }
+    else{
+        myFavoriteCats += `${cats[i]}, `;
+    }
+}
+
+console.log(myFavoriteCats); // My cats are called Pete, Biggles, and Jasmine.
